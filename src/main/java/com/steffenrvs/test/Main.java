0d98465222df00c2104ae32526b529f5bd78d2a7
sr9061s <steffenrvs@gmail.com>
2021-08-15 12:58:08 +0200

    Thread sleep

--- src/main/java/com/steffenrvs/test/Main.java
+++ src/main/java/com/steffenrvs/test/Main.java
@@ -1,7 +1,11 @@
 package com.steffenrvs.test;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            System.out.println("Hello World!");
+
+            Thread.sleep(5000);
+        }
     }
 }
